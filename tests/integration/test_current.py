ddcf7ad82f36fee7d23b0da584cd703350f10a8f
asmajabr <36918142+asmajabr@users.noreply.github.com>
2026-01-05 01:53:53 +0100

    Update test_current.py

--- tests/integration/test_current.py
+++ tests/integration/test_current.py
@@ -3,6 +3,7 @@
 from fastapi.testclient import TestClient
 from src.app import app
 import httpx
+from http import HTTPStatus
 import pytest
 
 client = TestClient(app)
@@ -10,7 +11,7 @@ client = TestClient(app)
 @pytest.fixture(autouse=True)
 def mock_open_meteo(monkeypatch):
     class MockResp:
-        status_code = 200
+        status_code = HTTPStatus.OK
         def json(self):
             return {
                 "current_weather": {
