5820cef14b938f480ac779790cfd2f1552de8b60
asmajabr <36918142+asmajabr@users.noreply.github.com>
2026-01-05 15:09:56 +0100

    tests: group imports in test_codes.py

--- tests/unit/test_codes.py
+++ tests/unit/test_codes.py
@@ -1,9 +1,10 @@
-
 # tests/unit/test_codes.py
+
+# Local
 from src.helpers import code_to_text
 
 def test_code_to_text_known_codes():
-    assert code_to_text(0) == "Clear"
+    assert code_to_text(0) == "Clear sky"
     assert code_to_text(1) == "Mainly clear"
 
 
